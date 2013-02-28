72c0508499b2cb72c311e669e6c2bcf526d428de
fanf <fanf@rocky>
2013-02-28 07:13:04 +0100

    added gitignore added assets folder with textures

---
+++ assets/Textures/WallPack2/Readme.txt
@@ -0,0 +1,6 @@
+This new walls is fo your TCs Add-ons use floedit and import them.
+
+Some walls have different names.Incleded SOD Walls to work on Wolfenstein Add-Ons,SOD Full
+SOD demo and Wolfestein Shawerare
+
+Giorgio
--- src/test/MainHelloWorld.java
+++ src/test/MainHelloWorld.java
@@ -1,16 +1,32 @@
 package test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.ImageIcon;
+
 import com.jme3.app.SimpleApplication;
+import com.jme3.asset.AssetInfo;
+import com.jme3.asset.AssetKey;
+import com.jme3.asset.AssetLocator;
+import com.jme3.asset.AssetManager;
+import com.jme3.asset.plugins.FileLocator;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import com.jme3.texture.Image;
+import com.jme3.texture.Texture;
+import com.jme3.texture.Texture2D;
+
+import de.lessvoid.nifty.tools.resourceloader.ResourceLocation;
 
 public class MainHelloWorld extends SimpleApplication {
 
@@ -26,7 +42,6 @@ public class MainHelloWorld extends SimpleApplication {
 
 	@Override
 	public void simpleInitApp() {
-		initKeys();
 		/** create a blue box at coordinates (1,-1,1) */
         Box box1 = new Box( Vector3f.ZERO, 1,1,1);
         Geometry blue = new Geometry("Box", box1);
@@ -37,26 +52,33 @@ public class MainHelloWorld extends SimpleApplication {
         blue.move(1,-1,1);
  
         /** create a red box straight above the blue one at (1,3,1) */
-        Box box2 = new Box( Vector3f.ZERO, 1,1,1);
+        Box box2 = new Box(1f,1f,1f);
         Geometry red = new Geometry("Box", box2);
-        Material mat2 = new Material(assetManager, 
-                "Common/MatDefs/Misc/Unshaded.j3md");
-        //mat2.setColor("Color", ColorRGBA.Red);
-        mat2.setTexture("ColorMap", 
-        	    assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
-        red.setMaterial(mat2);
+        
+        Material mat_lit = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        
+        assetManager.registerLocator("./assets", FileLocator.class);
+        
+        
+        String mywallFilename = "Textures/LightBrick/LightBrick-ColorMap.png";
+        Texture texture = assetManager.loadTexture(mywallFilename);
+        mat_lit.setTexture("DiffuseMap",texture);
+
+        mat_lit.setTexture("NormalMap", assetManager.loadTexture("Textures/LightBrick/LightBrick-NormalMap.png"));
+        mat_lit.setBoolean("UseMaterialColors",true);    
+        mat_lit.setColor("Specular",ColorRGBA.White);
+        mat_lit.setColor("Diffuse",ColorRGBA.White);
+        mat_lit.setFloat("Shininess", 5f); // [1,128]    
+        
+        
+        red.setMaterial(mat_lit);
         red.move(1,3,1);
+        rootNode.attachChild(red);
  
-        /** Create a pivot node at (0,0,0) and attach it to the root node */
-        Node pivot = new Node("pivot");
-        rootNode.attachChild(pivot); // put this node in the scene
- 
-        /** Attach the two boxes to the *pivot* node. */
-        pivot.attachChild(blue);
-        pivot.attachChild(red);
-        /** Rotate the pivot node: Note that both boxes have rotated! */
-        pivot.rotate(.4f,.4f,0f);
-		
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(1,0,-2).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun);
 	}
 	
 	 private void initKeys() {
