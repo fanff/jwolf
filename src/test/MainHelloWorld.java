0a5708b201660fe7655d7c2632075928d17eab82
fanff <fanff@example.com>
2013-03-04 13:48:58 +0100

    testing more light method

--- src/test/MainHelloWorld.java
+++ src/test/MainHelloWorld.java
@@ -22,12 +22,16 @@ import com.jme3.post.filters.CartoonEdgeFilter;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.control.Control;
+import com.jme3.scene.control.LightControl;
 import com.jme3.scene.shape.Box;
 import com.jme3.terrain.geomipmap.TerrainLodControl;
 import com.jme3.terrain.geomipmap.TerrainQuad;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
 import com.jme3.terrain.heightmap.ImageBasedHeightMap;
 import com.jme3.texture.Texture;
+import com.jme3.texture.Texture.MagFilter;
+import com.jme3.texture.Texture.MinFilter;
 import com.jme3.texture.Texture.WrapMode;
 
 public class MainHelloWorld extends SimpleApplication {
@@ -44,9 +48,10 @@ public class MainHelloWorld extends SimpleApplication {
 
 	}
 	JwLevel level = new JwLevel();
-	//private PssmShadowRenderer pssmRenderer;
+	
+	PointLight lamp=null;
 
-	//private DirectionalLightShadowRenderer pssmRenderer;
+	
 	@Override
 	public void simpleInitApp() {
 		flyCam.setMoveSpeed(10f);
@@ -58,62 +63,26 @@ public class MainHelloWorld extends SimpleApplication {
 		
 		Texture texture_blueCartoonHeight = assetManager.loadTexture("Textures/bluecartoon_height.bmp");
 		Texture texture_Blue17 = assetManager.loadTexture("Textures/WallPack/Blue17.bmp");
-		Texture texture_Grey131 = assetManager.loadTexture("Textures/WallPack/Grey13.bmp");
+		Texture texture_Grey13 = assetManager.loadTexture("Textures/WallPack/Grey13.bmp");
+		texture_Grey13.setMagFilter(MagFilter.Nearest);
+		texture_Grey13.setMinFilter(MinFilter.NearestNoMipMaps);
 		Texture bluecartoon_alpha = assetManager.loadTexture("Textures/bluecartoon_alpha.bmp");
 		
+		Texture texture_SP1 = assetManager.loadTexture("Textures/WallPack/Sp1.bmp");
+		texture_SP1.setMagFilter(MagFilter.Nearest);
+		texture_SP1.setMinFilter(MinFilter.NearestNoMipMaps);
+
+		Texture tex_bricksmall = assetManager.loadTexture("Textures/bricksmall.png");
+		tex_bricksmall.setMagFilter(MagFilter.Nearest);
+		tex_bricksmall.setMinFilter(MinFilter.NearestNoMipMaps);
 		
-
 		
-		Material	mat_terrain = new Material(assetManager, "Common/MatDefs/Terrain/Terrain.j3md");
-		mat_terrain.setTexture("Tex1", texture_blueCartoon);
-	    //mat_terrain.setTexture("Alpha", texture_blueCartoonHeight);
-
-		mat_terrain.setFloat("Tex1Scale", 1);
-
-
-		Material mat_stl = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat_stl.setTexture("ColorMap", texture_Blue17);
-		//mat_stl.setColor("Color", ColorRGBA.Blue);
+		Texture tex_normal = assetManager.loadTexture("Textures/bricksmall_normal.png");
+		tex_normal.setMagFilter(MagFilter.Nearest);
+		tex_normal.setMinFilter(MinFilter.NearestNoMipMaps);
 		
-		Material mat_cartoonheihtUnshaded = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat_cartoonheihtUnshaded.setTexture("ColorMap", bluecartoon_alpha);
-		mat_cartoonheihtUnshaded.setColor("Color", ColorRGBA.White);
-
-		Material mat_brick1 = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		String brick_color_filenae = "Textures/LightBrick/bricksmall.png";
-		String brick_normal_filename = "Textures/LightBrick/bricksmall_normal.png";
-		mat_brick1.setTexture("DiffuseMap",assetManager.loadTexture(brick_color_filenae));
-		mat_brick1.setTexture("NormalMap", assetManager.loadTexture(brick_normal_filename));
-		mat_brick1.setBoolean("UseMaterialColors",true);    
-		mat_brick1.setColor("Specular",ColorRGBA.White);
-		mat_brick1.setColor("Diffuse",ColorRGBA.White);
-		mat_brick1.setFloat("Shininess", 120f); // [1,128]    
-
-		Material ground_mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		ground_mat.setBoolean("UseMaterialColors", true);
-		ground_mat.setColor("Ambient",  ColorRGBA.DarkGray);
-		ground_mat.setColor("Diffuse",  ColorRGBA.DarkGray);
-		ground_mat.setColor("Specular", ColorRGBA.White);
-		ground_mat.setFloat("Shininess", 12);
-
-		////
-		Material mat_brick_blue5 = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		String brick3_color_filenae = "Textures/WallPack/Blue5.bmp";
-		String brick3_normal_filename = "Textures/LightBrick/bricksmall_normal.png";
-		mat_brick_blue5.setTexture("DiffuseMap",assetManager.loadTexture(brick3_color_filenae));
-		mat_brick_blue5.setTexture("NormalMap", assetManager.loadTexture(brick3_normal_filename));
-		mat_brick_blue5.setBoolean("UseMaterialColors",false);    
-		mat_brick_blue5.setColor("Specular",ColorRGBA.White);
-		mat_brick_blue5.setColor("Diffuse",ColorRGBA.White);
-		mat_brick_blue5.setFloat("Shininess", 1f); // [1,128]    
-
-		Material mat_brick_cartoon = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		mat_brick_cartoon.setTexture("DiffuseMap",texture_blueCartoon);
-		mat_brick_cartoon.setTexture("NormalMap", assetManager.loadTexture(brick3_normal_filename));
-		mat_brick_cartoon.setBoolean("UseMaterialColors",false);    
-		mat_brick_cartoon.setColor("Specular",ColorRGBA.White);
-		mat_brick_cartoon.setColor("Diffuse",ColorRGBA.White);
-		mat_brick_cartoon.setFloat("Shininess", 50f); // [1,128]    
+		
+		
 		
 		AbstractHeightMap heightmap = null;
 	    heightmap = new ImageBasedHeightMap(texture_blueCartoonHeight.getImage());
@@ -142,15 +111,7 @@ public class MainHelloWorld extends SimpleApplication {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		/** create the ground 
-		Box ground = new Box(256f,1f,256f);
-		Geometry ground_geom = new Geometry("Box", ground);
-		TangentBinormalGenerator.generate(ground_geom.getMesh(), true);
-		ground_geom.setMaterial(ground_mat);
-		ground_geom.move(256f,-2,256f);
-		rootNode.attachChild(ground_geom);
-*/
+		
 		//
 		//load the level
 		//
@@ -160,10 +121,12 @@ public class MainHelloWorld extends SimpleApplication {
 				if(walldef != null){
 					if(walldef.equals("#")){
 						//box
-						createUnnshadedBox(x, 0,y, texture_Blue17);
+						//createUnnshadedBox(x, 0,y, texture_SP1);
+						
+						createBoxShadedBox(x, 0, y, tex_bricksmall, tex_normal);
 					}else if(walldef.equals(" ")){
 						//ground
-						createUnnshadedBox(x, -1,y, texture_Grey131);
+						createUnnshadedBox(x, -1,y, texture_Grey13);
 						//createBox(x, -1, y, mat_brick_cartoon);
 					}
 					else if(walldef.equals("l")){
@@ -173,9 +136,17 @@ public class MainHelloWorld extends SimpleApplication {
 				}
 			}
 		}
+		
+		lamp = new PointLight();
+		lamp.setPosition(new Vector3f( 5f,1.2f, 5f));
+		lamp.setRadius(30f);
+		lamp.setColor(ColorRGBA.White);
+		rootNode.addLight(lamp);
+		
+		
 		//createBox(0, 0, mat_brick1);
 		AmbientLight al = new AmbientLight();
-		al.setColor(ColorRGBA.White.mult(30f));
+		al.setColor(ColorRGBA.White.mult(3f));
 		rootNode.addLight(al);
 
 		FilterPostProcessor fpp=new FilterPostProcessor(assetManager);
@@ -184,7 +155,7 @@ public class MainHelloWorld extends SimpleApplication {
 		toon.setEdgeIntensity(1.0f);
 		toon.setNormalThreshold(0.8f);
 		fpp.addFilter(toon);
-		///viewPort.addProcessor(fpp);
+		//viewPort.addProcessor(fpp);
 
 		/**
         DirectionalLight sun = new DirectionalLight();
@@ -200,39 +171,60 @@ public class MainHelloWorld extends SimpleApplication {
 
 
 	}
-
+	public void simpleUpdate(float tpf) {
+    
+		cam.getLocation();
+		lamp.setPosition(new Vector3f(cam.getLocation().x,1.2f,cam.getLocation().z));
+	}
+	
 	int boxCounter = 0;
 
 	private void createPointLight(int x,int y){
 		PointLight lamp = new PointLight();
 		lamp.setPosition(new Vector3f(2f*x ,1.2f, 2f*y));
-		lamp.setRadius(30f);
+		lamp.setRadius(10f);
 		lamp.setColor(ColorRGBA.White);
 		rootNode.addLight(lamp);
+		
 	}
 	
 	private void createUnnshadedBox(int x,int z,int y,Texture Texture){
-		Material mat_cartoonheihtUnshaded = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat_cartoonheihtUnshaded.setTexture("ColorMap", Texture);
-		mat_cartoonheihtUnshaded.setColor("Color", ColorRGBA.White);
+		Material unshadedMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		unshadedMaterial.setTexture("ColorMap", Texture);
+		unshadedMaterial.setColor("Color", ColorRGBA.White);
 		
 		boxCounter++;
 		Box box3 = new Box(1f,1f,1f);
 		Geometry geom_box3 = new Geometry("Box"+boxCounter, box3);
-		geom_box3.setMaterial(mat_cartoonheihtUnshaded);
+		geom_box3.setMaterial(unshadedMaterial);
 		geom_box3.move(2f*x ,z*2f, 2f*y);
 		rootNode.attachChild(geom_box3);
+		
 		//makeToonish(geom_box3);
 	}
 	
-	
-	private void createBox(int x,int z,int y,Material boxMaterial){
+	//
+	private void createBoxShadedBox(int x,int z,int y,Texture Texture,Texture normalMap){
+		Material mat_lighting = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+		mat_lighting.setTexture("DiffuseMap",Texture);
+		mat_lighting.setTexture("NormalMap", normalMap);
+		
+		mat_lighting.setBoolean("UseMaterialColors",false);
+		
+		mat_lighting.setBoolean("LowQuality",true);
+		
+		mat_lighting.setColor("Specular",ColorRGBA.White);
+		mat_lighting.setColor("Diffuse",ColorRGBA.White);
+		mat_lighting.setFloat("Shininess", 50f); // [1,128]    
+		
 		boxCounter++;
 		Box box3 = new Box(1f,1f,1f);
 		Geometry geom_box3 = new Geometry("Box"+boxCounter, box3);
-		geom_box3.setMaterial(boxMaterial);
+		geom_box3.setMaterial(mat_lighting);
 		geom_box3.move(2f*x ,z*2f, 2f*y);
 		rootNode.attachChild(geom_box3);
+		
+		
 		//makeToonish(geom_box3);
 	}
 
